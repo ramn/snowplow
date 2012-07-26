b004d5e9176d4731951362e110af3651c32fd488
Yali Sassoon <yali.sassoon@keplarllp.com>
2012-07-26 15:28:54 +0100

    updated etl.py so trawls through relevant subfolders and parses files. still a lot of work needs to be done

--- infobright/etl.py
+++ infobright/etl.py
@@ -1,43 +1,25 @@
-import optparse
-p = optparse.OptionParser()
+#***********************************************************************************************************
+# FUNCTIONS DEPLOYED IN PROGRAMME FLOW
+# ***********************************************************************************************************
 
-# output file
-p.add_option("-o", action="store", dest="outfile")
-p.add_option("--output", action="store", dest="outfile")
-
-# source file
-p.add_option("-i", dest="infile")
-p.add_option("--input", dest="infile")
-
-# parse the command line
-opts, args = p.parse_args()
-
-# retrieve the options settings
-outfile = opts.outfile
-infile = opts.infile 
-
-print "infile is ",  infile
-print "outfile is ", outfile
-
-i = open(infile)
-line = i.readline()
-
-while line:
-	# parse the input line of data, creating a list of fields each delimited by the ctrl-a character (chr(1))
-	fieldsList = line.split(chr(1)) 
-	fieldsString = '\t'.join(map(str, fieldsList))
-	print fieldsString
+# following function looks in specified directory and returns a list of folders
+# TO DO: update function so that pattern matches on directory name and only returns folders that have names of the correct form i.e. 'dt=YYYY-MM-DD'
+import os
+def get_eligible_subdirectories(dir):
+    return [name for name in os.listdir(dir)
+            if os.path.isdir(os.path.join(dir, name))]
 
 # processLine takes an input line of Hive data (ctrl-a delimited with browser features packed into an array) 
 # and returns a tab delimited string suitable for import into InfoBright
 def processLine(inputLine):
-	# check that the line of data corresponds to the expected format (i.e. X fields, each ctrl-a delimited)
+	# check that the line of data corresponds to the expected format (i.e. 24 fields, each ctrl-a delimited)
 	
 	# create a list, where each field in the original line of data constitues one element in the list
 	fieldsList = inputLine.split(chr(1))
 
 	# convert the browser features array (the Xth element in the list) into a single tab delimited string
-	fieldsList[X] = convertBrowserFeatures(fieldsList[X])
+	fieldsList[24] = convertBrowserFeatures(fieldsList[24])
+	# NOTE: is there a better way to do this than using a mutable list?
 
 	# then convert the list into a tabl delimited string suitable to be imported into InfoBright
 	outputLine = '\t'.join(map(str, fieldsList))
@@ -53,44 +35,109 @@ def convertBrowserFeatures(inputFeatures):
 	browserFeaturesList = inputFeatures.split(chr(2))
 
 	# now check for the existence of each browser feature, and assign the appropriate value to the feature variable
-	br_features_pdf = false
-	br_features_flash = false
-	br_features_java = false
-	br_features_director = false
-	br_features_quicktime = false
-	br_features_realplayer = false
-	br_features_windowsmedia - false
-	br_features_gears = false
-	br_features_silverlight = false
+	br_features_pdf = False
+	br_features_flash = False
+	br_features_java = False
+	br_features_director = False
+	br_features_quicktime = False
+	br_features_realplayer = False
+	br_features_windowsmedia = False
+	br_features_gears = False
+	br_features_silverlight = False
 
 	if 'pdf' in browserFeaturesList:
-		br_features_pdf = true
+		br_features_pdf = True
 
 	if 'flash' in browserFeaturesList:
-		br_features_flash = true
+		br_features_flash = True
 
 	if 'java' in browserFeaturesList:
-		br_features_java = true
+		br_features_java = True
 
 	if 'director' in browserFeaturesList:
-		br_features_director = true
+		br_features_director = True
 
-	if 'quicktime' in browserFeaturesList
-		br_features_quicktime = true
+	if 'quicktime' in browserFeaturesList:
+		br_features_quicktime = True
 
 	if 'realplayer' in browserFeaturesList:
-		br_features_realplayer = true
+		br_features_realplayer = True
 
 	if 'wma' in browserFeaturesList:
-		br_features_windowsmedia = true
+		br_features_windowsmedia = True
 
 	if 'gears' in browserFeaturesList:
-		br_features_gears = true
+		br_features_gears = True
 
 	if 'silverlight' in browserFeaturesList:
-		br_features_silverlight = true
+		br_features_silverlight = True
 
 	# now concatenate the results together into a single tab delimited string 
-	outputBrowserFeatures = str(br_features_pdf) + '\t' + str(br_features_flash) + '\t' str(br_features_java) +	'/t' + str(br_features_director) + '/t' + str(br_features_quicktime) + '/t' + str(br_features_realplayer) + '/t' + str(br_features_windowsmedia) + '/t' + str(br_features_gears) + '/t' + str(br_features_gears) + '/t' + br_features_silverlight
+	outputBrowserFeatures = str(br_features_pdf) + '\t' + str(br_features_flash) + '\t' + str(br_features_java) + '\t' + str(br_features_director) + '\t' + str(br_features_quicktime) + '\t' + str(br_features_realplayer) + '\t' + str(br_features_windowsmedia) + '\t' + str(br_features_gears) + '\t' + str(br_features_gears) + '\t' + str(br_features_silverlight)
 
 	return outputBrowserFeatures
+
+
+
+# ***********************************************************************************************************
+# PROGRAM FLOW BEGINS HERE
+# ***********************************************************************************************************
+
+
+# optparse used to handle parameters passed in at the command line
+import optparse
+p = optparse.OptionParser()
+
+# output file
+p.add_option("-o", action="store", dest="outfile")
+p.add_option("--output", action="store", dest="outfile")
+
+# source folder
+p.add_option("-i", dest="infolder")
+p.add_option("--input", dest="infolder")
+
+# parse the command line
+opts, args = p.parse_args()
+
+# retrieve the options settings
+outfile = opts.outfile
+inputFolder = opts.infolder 
+
+# Create an output file
+o = open(outfile, 'w')
+
+listOfSubFolders = get_eligible_subdirectories(inputFolder)
+for subFolder in listOfSubFolders:
+	print "Processing folder ", subFolder
+	# check if file name matches
+	# TO WRITE APPROPRIATE FUNCTION i.e. if check(folder) then: ...
+	
+	listOfFiles = os.listdir(inputFolder + '/' + subFolder)
+	for file in listOfFiles:
+		i = open(inputFolder + '/' + subFolder + '/' + file, 'r')
+		print "Processing file" + inputFolder + '/' + subFolder + '/' + file
+		inputLine = i.readline()
+		while inputLine:
+			o.writelines(processLine(inputLine))
+			inputLine = i.readline()
+
+'''
+# Open the input file
+i = open(infolder, 'r')
+inputLine = i.readline()
+
+counter = 0
+while inputLine:
+	# new, functional approach:
+	o.writelines(processLine(inputLine))
+
+	# print status update
+	counter = counter + 1
+	print "written line ", counter
+
+	inputLine = i.readline()
+	
+# close input and output file
+'''
+i.close()
+o.close()
