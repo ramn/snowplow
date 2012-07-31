0985f3d1bcd583ea4a0c83dcc955ebd7a738c957
Yali Sassoon <yali.sassoon@keplarllp.com>
2012-07-31 20:13:26 +0100

    updated function for processing query string

--- 3-etl/infobright/etl-raw-logs.py
+++ 3-etl/infobright/etl-raw-logs.py
@@ -5,10 +5,7 @@
 # processLine parses the line and returns a string of tab delimited fields suitable for import into Infobright / Postgres
 import re
 def processLine(inputLine):
-
-
 	# Decompose the line into list using each tab delimited separator
-
 	fieldsList = inputLine.split('\t')
 	for field in fieldsList:
 		print "field = ", field
@@ -24,13 +21,35 @@ def processLine(inputLine):
 		uri_query = fieldsList[11]
 
 		# commenting out below line for testing purposes (haven't decided which user agent utils library to use)	
-		# browserAndOsDetails = processUserAgent(user_agent) # browserAndOsDetails is a map containing descriptors of the browser and OS (TODO: list fields in comment for clarity)
+		
+		browserAndOsDetails = processUserAgent(user_agent) # browserAndOsDetails is a map containing descriptors of the browser and OS (TODO: list fields in comment for clarity)
 
-		queryFields = processUriQuery(uri_query) # the remaining fields are packed in this map (TODO: list fields in comment for clarity)
+		queryFields = processUriQuery(uri_query) # fields from the query string are packed in this dictionary
+
+		# Map snowplow.js query parameters to associated fields in Hive / Infobright / PostgreSql
+		txn_id 			= queryFields['tid']
+		user_id 		= queryFields['uid']
+		visit_id 		= queryFields['vid']
+		lang 			= queryFields['br_lang']
+		cookie 			= queryFields['br_cookie']
+		page_referrer 	= queryFields['refr']
+		page_url 		= queryFields['url']
+		ev_category 	= queryFields['ev_ca']
+		ev_action 		= queryFields['ev_ac']
+		ev_label 		= queryFields['ev_la']
+		ev_property 	= queryFields['ev_pr']
+		ev_value 		= queryFields['ev_va']
+		mkt_source		= queryFields['utm_source']
+		mkt_medium		= queryFields['utm_medium']
+		mkt_campaign	= queryFields['utm_campaign']
+		mkt_term		= queryFields['utm_term']
+		mkt_content		= queryFields['utm_content'] 
+
+		return # return a string of tab delimited fields, by concatenating together
 
 	else:
 		# handle the error (log it to a file? or just ignore the line)
-		return 0
+		return None
 
 
 # checks if the format of the line of data is as expected, and handles the error if not
@@ -58,48 +77,29 @@ def processUserAgent(user_agent):
 	# insert code here, leverage existing library (Google for best)
 	return 
 
+# processUriQuery takes the complete query string and returns a complete dictionary with each recognised element set
 def processUriQuery(queryFields):
 	
-	# Decompose the query string based on the '&' character into a a key value map, using the "=" in the string to differentiate keys from values
-	queryList = queryFields[1:].split('&') # split the list of queries into separate key=value pairs, each of which forms an item in a list. Note - we drop the first character ('&') so that we don't start the list with an empty item
+	# Decompose the query string based on the '&' character into a list where each element is of the form 'queryParameter=value'. 
+	queryList = queryFields.split('&') 
 
-	# Now create convert the list of parameters in the form (key=value) into a dictionary where each entry takes the form (key, value)
-	queryDictionary = map(splitKeyValue, queryList)
+	# Filter the list so it only contains fields that contain '='
+	containsEquals = lambda x: "=" in x
+	queryListFiltered = filter(containsEquals, queryList)
 
-	# Now extract the relevant fields from the query string, or set the values to NULL if they are not present
-	possibleParametersList = ["tid", "uid", "vid", "lang", "cookie", "refr", "page", "url", "ev_ca", "ev_la", "ev_pr", "ev_va"]
+	# Convert the list of parameters in the form (key=value) into a dictionary where each entry takes the form (key, value)
+	queryDictionary = dict(map(splitKeyValue, queryList))
 
-	# Query string parameters map onto those in the Hive table definition as follows:
-	# tid = txn_id 
-	# uid = user_id
-	# vid = visit_id (really visit counter)
-	# lang = br_lang
-	# cookie = br_cookies
-	# refr = page_referrer
-	# url = page_url
-	# page = page_title
-	# ev_ca = ev_category
-	# ev_ac = ev_action
-	# ev_la = ev_label
-	# ev_pr = ev_property
-	# ev_va = ev_value 
-	# utm_source = mkt_source
-	# utm_medium = mkt_medium
-	# utm_term = mkt_term
-	# utm_content = mkt_content
-	# utm_campaign = mkt_name
-	
+	# If any of the fields are NOT present in the dictionary, add them and set the values to NULL
+	possibleParameters = ["tid", "uid", "vid", "lang", "cookie", "refr", "page", "url", "ev_ca", "ev_la", "ev_pr", "ev_va", "f_pdf", "f_fla", "f_java", "f_dir", "f_qt", "f_realp", "f_wma", "f_gears", "f_ag"]
 
-	browserFeatureList = ["pdf", "fla", "java", "dir", "qt", "realp", "wma", "gears", "ag"]
+	for p in possibleParameters:
+		if not(p in queryDictionary) then queryDictionary[p] = None
 
-
-	# Note: do we want someway to capture query values that do not live in the schema? 
-	return
+	return queryDictionary
 
 # splits a query parameter of the form key=value into a tuple (key, value)
-def splitKeyValue(queryParameter):
-	(key, value) = queryParameter.split('=')
-	return key, value
+
 
 
 ##################################
@@ -108,6 +108,7 @@ def splitKeyValue(queryParameter):
 
 sampleDataLine1 = r'2012-07-23	18:01:25	DUB2	3402	82.23.113.162	GET	d3gs014xn8p70.cloudfront.net	/ice.png	200	https://www.psychicbazaar.com/shop/checkout/	Mozilla/5.0%20(iPhone;%20CPU%20iPhone%20OS%205_1_1%20like%20Mac%20OS%20X)%20AppleWebKit/534.46%20(KHTML,%20like%20Gecko)%20Version/5.1%20Mobile/9B206%20Safari/7534.48.3	&ev_ca=ecomm&ev_ac=checkout&ev_la=email&ev_pr=ERROR&tid=534263&uid=71b74fd331fa1eb5&vid=1&lang=en-gb&refr=http%253A%252F%252Fwww.psychicbazaar.com%252F4-jewellery&f_pdf=0&f_qt=1&f_realp=0&f_wma=0&f_dir=0&f_fla=0&f_java=0&f_gears=0&f_ag=0&res=320x480&cookie=1&url=https%253A%252F%252Fwww.psychicbazaar.com%252Fshop%252Fcheckout%252F'
 sampleDataLine2 = r'2012-07-23	18:01:25	DUB2	3402	82.23.113.162	GET	d3gs014xn8p70.cloudfront.net	/ice.png	200	https://www.psychicbazaar.com/shop/checkout/	Mozilla/5.0%20(iPhone;%20CPU%20iPhone%20OS%205_1_1%20like%20Mac%20OS%20X)%20AppleWebKit/534.46%20(KHTML,%20like%20Gecko)%20Version/5.1%20Mobile/9B206%20Safari/7534.48.3	&ev_ca=ecomm&ev_ac=checkout&ev_la=paypal-button&tid=109138&uid=71b74fd331fa1eb5&vid=1&lang=en-gb&refr=http%253A%252F%252Fwww.psychicbazaar.com%252F4-jewellery&f_pdf=0&f_qt=1&f_realp=0&f_wma=0&f_dir=0&f_fla=0&f_java=0&f_gears=0&f_ag=0&res=320x480&cookie=1&url=https%253A%252F%252Fwww.psychicbazaar.com%252Fshop%252Fcheckout%252F'
+sampleDataLine3 = r'2012-07-24	16:54:50	LAX3	3448	64.60.168.146	GET	d3gs014xn8p70.cloudfront.net	/ice.png	200	https://www.psychicbazaar.com/shop/checkout/	Mozilla/5.0%20(iPhone;%20CPU%20iPhone%20OS%205_1_1%20like%20Mac%20OS%20X)%20AppleWebKit/534.46%20(KHTML,%20like%20Gecko)%20Version/5.1%20Mobile/9B206%20Safari/7534.48.3	page=Psychic%2520Bazaar%2520Checkout&tid=654477&uid=5636c31749a19950&vid=1&lang=en-us&refr=http%253A%252F%252Fwww.psychicbazaar.com%252Fsearch%253Fsearch_query%253DEden&f_pdf=0&f_qt=1&f_realp=0&f_wma=0&f_dir=0&f_fla=0&f_java=0&f_gears=0&f_ag=0&res=320x480&cookie=1&url=https%253A%252F%252Fwww.psychicbazaar.com%252Fshop%252Fcheckout%252F'
 
 
 
